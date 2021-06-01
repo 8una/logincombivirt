25b85b46ae019f431f3c75a4b48e85b1774aeaf6
Bar <you@example.com>
2021-06-01 13:56:25 -0300

    cosas para admin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use App\Role;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
@@ -75,14 +75,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'lastname' => $data['lastname'],
             'DNI' => $data['DNI'],
             'edad' => $data['edad'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            
-        ]);
+        ]);        
+        $user->roles()->attach(Role::where('name', 'user')->first());  
+    
+        return $user;
+        
     }
 }
