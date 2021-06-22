48b410f08ad84584ae1c44b2a6ee3fa5ca6fbcc6
Bar <you@example.com>
2021-06-22 15:23:17 -0300

    Home arreglado

--- app/User.php
+++ app/User.php
@@ -46,31 +46,68 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class)->withTimestamps();
     }
-    public function authorizeRoles($roles)
-    {
-        abort_unless($this->hasAnyRole($roles), 401);
-        return true;
-    }    public function hasAnyRole($roles)
-    {
-        if (is_array($roles)) {
-            foreach ($roles as $role) {
-                if ($this->hasRole($role)) {
-                    return true;
-                }
-            }
-        } else {
-            if ($this->hasRole($roles)) {
-                 return true; 
-            }   
-        }
-        return false;
-    }
+    // public function authorizeRoles($roles)
+    // {
+    //     abort_unless($this->hasAnyRole($roles), 401);
+    //     return true;
+    // }    public function hasAnyRole($roles)
+    // {
+    //     if (is_array($roles)) {
+    //         foreach ($roles as $role) {
+    //             if ($this->hasRole($role)) {
+    //                 return true;
+    //             }
+    //         }
+    //     } else {
+    //         if ($this->hasRole($roles)) {
+    //              return true; 
+    //         }   
+    //     }
+    //     return false;
+    // }
     
-    public function hasRole($role)
-    {
-        if ($this->roles()->where('name', $role)->first()) {
+    // public function hasRole($role)
+    // {
+    //     if ($this->roles()->where('name', $role)->first()) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    public function authorizeRoles($roles) {
+    if ($this->hasAnyRole($roles)) {
+        return true;
+    }
+    abort(401, 'Esta acción no está autorizada.');
+}
+
+public function hasAnyRole($roles) {
+    if (is_array($roles)) {
+        foreach ($roles as $role) {
+            if ($this->hasRole($role)) {
+                return true;
+            }
+        }
+    } else {
+        if ($this->hasRole($roles)) {
             return true;
         }
-        return false;
     }
+    return false;
+}
+
+public function hasRole($role) {
+    if ($this->roles()->where('name', $role)->first()) {
+        return true;
+    }
+    return false;
+}
+public function isAdmin() {
+   return $this->roles()->where('name', 'admin')->exists();
+}
+
+public function isUser() {
+   return $this->roles()->where('name', 'user')->exists();
+}
+
 }
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,32 +1,30 @@
-
-        
-  <div class="d-flex bg-dark">
+       <div class="d-flex bg-dark">
    
      <div>
         <div class="text-light">
-         
-         @guest{{-- ACA VAN LAS COSAS EN LA NAVEGACION QUE SE LE MUESTRAN A UNA PERSONA NO LOGEADA--}}
+
+            
+            @guest
             <button class="btn btn-dark btn-lg "><div class="ml-2" ><a class="text-light" href="{{route('home')}}"> Inicio</a></div></button>
             <button class="btn btn-dark btn-lg "><div class="ml-2" ><a class="text-light" href=""> Buscar Viaje</a></div></button>
             <button class="btn btn-dark btn-lg "><div class="ml-2" ><a class="text-light" href=""> Subscripcion</a></div></button>
-         @endguest
-         {{-- ACA LAS DE USUARIO REGISTRADO--}}
-         @auth
+            @else
+            @if(Auth::user()->isAdmin())
             <button class="btn btn-dark btn-lg"><div class="ml-2" ><a class="text-light" href="{{route('home')}}"> Inicio</a></div></button>
             <button class="btn btn-dark btn-lg"><form action="" method="get">@csrf<div class="ml-2"><a class="text-light" href="{{route('misViajes', Auth::user()->dni)}}"> Mis viajes </a></form></div></button>
             <button class="btn btn-dark btn-lg"><form action="" method="get">@csrf<div class="ml-2"><a href="{{route('misViajesPasados', Auth::user()->dni)}}" class="text-light"> Historial </a></form></div></button>
             <button class="btn btn-dark btn-lg"><div class="ml-2"><a href="{{route('subscribirse')}}" class="text-light"> Subscripcion </a></div></button>
-         @endauth
-      
-         {{--   
-      @if($request->user()->authorizeRoles(['admin']))
-         <button class="btn btn-dark btn-lg"><div class="ml-2"><a href="{{route('uAdmin')}}" class="text-light"> Administración</a></div></button> 
-      
-      @elseif (Auth::user()->authorizeRoles(['1']))
-         <button class="btn btn-dark btn-lg"><div class="ml-2"><a href="{{route('uAdmin')}}" class="text-light"> Administración</a></div></button>
-      @endif  --}}
-
+            <button class="btn btn-dark btn-lg"><div class="ml-2"><a href="{{route('uAdmin')}}" class="text-light"> Administración</a></div></button>
+            @endif
+            @if(Auth::user()->isUser())
+            <button class="btn btn-dark btn-lg"><div class="ml-2" ><a class="text-light" href="{{route('home')}}"> Inicio</a></div></button>
+            <button class="btn btn-dark btn-lg"><form action="" method="get">@csrf<div class="ml-2"><a class="text-light" href="{{route('misViajes', Auth::user()->dni)}}"> Mis viajes </a></form></div></button>
+            <button class="btn btn-dark btn-lg"><form action="" method="get">@csrf<div class="ml-2"><a href="{{route('misViajesPasados', Auth::user()->dni)}}" class="text-light"> Historial </a></form></div></button>
+            <button class="btn btn-dark btn-lg"><div class="ml-2"><a href="{{route('subscribirse')}}" class="text-light"> Subscripcion </a></div></button>
+            @endif  
+            @endguest
       </div>        
    </div>
    </div>
-   @yield('content2')
+   @yield('content2') 
+
